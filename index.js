e2d78e5f7b4a979026381f3a0fe7de49f14a63e2
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2020-12-29 21:15:25 +0800

    frontend design overhaul

--- index.js
+++ index.js
@@ -22,8 +22,21 @@ app.get("/", (req, res) => {
   res.sendFile("home.html", { root: __dirname });
 });
 
-app.get("/actors", (req, res) => {
-  pool.query(SELECT_ALL_ACTORS, function (error, results, fields) {
+const pageLimit = 20;
+
+app.get("/name", (req, res) => {
+  var strSearch = req.query.strSearch;
+  var pageNum = req.query.pageNum - 1;
+  var offset = pageNum * pageLimit;
+  var query =
+    'SELECT name FROM movies WHERE name LIKE "%' +
+    strSearch +
+    '%" LIMIT ' +
+    pageLimit +
+    " OFFSET " +
+    offset;
+
+  pool.query(query, function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });
@@ -32,3 +45,30 @@ app.get("/actors", (req, res) => {
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+/**
+Single Query: (Single-table query must be written for the largest table (degree > 10) only.)
+1. Find movie by name
+SELECT name FROM movies
+WHERE name LIKE "%star wars%"
+LIMIT 20 OFFSET 0
+
+2. Find movie by id
+SELECT name FROM movies
+WHERE movies.id = 0
+
+3. Find movies by year shown
+SELECT name FROM movies
+WHERE YEAR = 2000
+LIMIT 20 OFFSET 0
+
+4. Find movies by finding less than or equal to the rating specified
+SELECT name FROM movies
+WHERE movies.rank < 5.0
+LIMIT 20 OFFSET 0
+
+4. Find movies by finding more than or equal to the rating specified
+SELECT name FROM movies
+WHERE movies.rank < 5.0
+LIMIT 20 OFFSET 0
+ */
